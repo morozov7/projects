d74557d442d30beed2885a7128d5ed6fdfc28206
morozov7 <morozov7@proton.me>
2022-10-12 12:36:49 -0300

    improving2

--- arith_geo.c
+++ arith_geo.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-float $1(float a, float b, float* z, float* y, float* x, float* p, float* k, float* r, float* n){ 
+float $1(float a, float b, float* $3, float* $4, float* $5, float* $6, float* $7, float* $8, float* $9){ 
     
     float $2;
     $2 = a + b;
-    *z = a - b;
-    *y = a * b;
-    *x = a / b;
-    *p = pow(a, b);
-    *n = pow(b, a);
-    *k = sqrt(a);
-    *r = sqrt(b);
+    *$3 = a - b;
+    *$4 = a * b;
+    *$5 = a / b;
+    *$6 = pow(a, b);
+    *$7 = pow(b, a);
+    *$8 = sqrt(a);
+    *$9 = sqrt(b);
 
     return $2;
 }
 void main(){
     
-    float z, y, a, b, p, k, r, x, o, n, pm; 
-    printf("chose 2 numbers{n1, n2 ∈ {R*}}\n:");
+    float a, b, $3, $4, $5, $6, $7, $8, $9; 
+    printf("chose 2 numbers{n1, n2 ∈ {R - 0}}\n:");
     scanf("%f %f",&a,&b);
-    printf("sum:\t%f\n", $1(a, b, &z, &y, &x, &p, &k, &r, &n));
-    printf("sub:\t%f\n", z);
-    printf("prod:\t%f\n", y);
-    printf("div:\t%f\n", x);
-    printf("n1 power n2:\t%f\n", p);
-    printf("n2 power n1:\t%f\n", n);
-    printf("sqrt n1:\t%f\n", k);
-    printf("sqrt n2:\t%f\n", r);                                                                                                    // freak--
+    printf("sum:\t%f\n", $1(a, b, &$3, &$4, &$5, &$6, &$7, &$8, &$9));
+    printf("sub:\t%f\n", $3);
+    printf("prod:\t%f\n", $4);
+    printf("div:\t%f\n", $5);
+    printf("n1 power n2:\t%f\n", $6);
+    printf("n2 power n1:\t%f\n",$7);
+    printf("sqrt n1:\t%f\n", $8);
+    printf("sqrt n2:\t%f\n", $9);                                                                                                    // freak--
 }
